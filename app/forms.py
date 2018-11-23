67319aeca5680d423b50142b929a2a1cfe4342e9
EC2 Default User <ec2-user@ip-172-31-32-112.us-east-2.compute.internal>
2018-11-23 05:25:07 +0000

    11232

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,3 @@
 from django import forms
 
 
-class po(forms.Form):
-    your_name = forms.CharField(
-        label='名前',
-        max_length=20,
-        required=True,
-        widget=forms.TextInput()
-    )
