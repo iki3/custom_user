0e168c66357a490f6349412ec7ac629f1ad47b68
EC2 Default User <ec2-user@ip-172-31-32-112.us-east-2.compute.internal>
2018-12-29 13:57:16 +0000

    122914

--- english_test/settings.py
+++ english_test/settings.py
@@ -124,11 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [                  # <-- ここから
-    os.path.join(BASE_DIR, 'static'),
-]  
-
 # カスタムユーザーを使う
 AUTH_USER_MODEL = 'app.User'
 
