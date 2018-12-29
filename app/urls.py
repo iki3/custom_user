dc3e16f5ae9911b8c6b8a1eccc6df38b7d3eb74f
EC2 Default User <ec2-user@ip-172-31-32-112.us-east-2.compute.internal>
2018-12-29 13:18:05 +0000

    12298

--- app/urls.py
+++ app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('user_create/complete/<token>/', views.UserCreateComplete.as_view(), name='user_create_complete'),
     path('user_detail/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
     path('user_update/<int:pk>/', views.UserUpdate.as_view(), name='user_update'),
+    
     path('password_change/', views.PasswordChange.as_view(), name='password_change'),
     path('password_change/done/', views.PasswordChangeDone.as_view(), name='password_change_done'),
     path('password_reset/', views.PasswordReset.as_view(), name='password_reset'),
